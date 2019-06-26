7b618a2bf46e32efef1dd91a8db22657416203fa
YamadaKyohei <kyohei.yamada@vogue.is.uec.ac.jp>
2019-06-26 12:37:36 +0900

    Change instance name

--- learningXOR.py
+++ learningXOR.py
@@ -4,38 +4,38 @@ import numpy as np
 from sklearn.neural_network import MLPRegressor
 
 def main():
-    clf = MLPRegressor(hidden_layer_sizes=(3),max_iter=10000,activation='relu',verbose=True,learning_rate_init=0.01)
+    model = MLPRegressor(hidden_layer_sizes=(3),max_iter=10000,activation='relu',verbose=True,learning_rate_init=0.01)
 
     # 説明変数
     X = [[0,0],[0,1],[1,0],[1,1]]
     # 目的変数
     Y = [0,1,1,0]
     # 学習
-    clf.fit(X, Y)
+    model.fit(X, Y)
     
     # 学習結果で推論
-    pred = clf.predict(X)
+    pred = model.predict(X)
     print(pred)
 
     # 学習結果表示
 
-    #print(clf.coefs_)#これでweightが見れる！
-    #print(clf.intercepts_)#これでbiasが見れる！
+    #print(model.coefs_)#これでweightが見れる！
+    #print(model.intercepts_)#これでbiasが見れる！
     
     print("// ===== ここからコピペ =====")
     
     print("const float w[depth-1][MaxNode][MaxNode] = {")
-    for i in range(len(clf.coefs_)):
+    for i in range(len(model.coefs_)):
         if i is not 0:
             print(", ")
-        print(np.array2string(clf.coefs_[i],separator=', ').replace("[","{").replace("]","}"),end="")
+        print(np.array2string(model.coefs_[i],separator=', ').replace("[","{").replace("]","}"),end="")
     print("")
     print("};")
     print("const float bias[depth-1][MaxNode] = {")
-    for i in range(len(clf.intercepts_)):
+    for i in range(len(model.intercepts_)):
         if i is not 0:
             print(", ")
-        print(np.array2string(clf.intercepts_[i],separator=', ').replace("[","{").replace("]","}"), end="")
+        print(np.array2string(model.intercepts_[i],separator=', ').replace("[","{").replace("]","}"), end="")
     print("")
     print("};")
     
